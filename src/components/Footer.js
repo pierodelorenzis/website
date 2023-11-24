4ff650f2d6482297884c4d55d84349276526371f
pierodelorenzis <delorenzispiero@gmail.com>
2023-11-24 20:23:39 +0100

    - Reformat code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,23 @@
-import { Container, Row, Col } from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
 
 var year = new Date().getFullYear()
 
 export const Footer = () => {
-  return (
-    <div className="footer">
+    return (
+        <div className="footer">
 
-        <Row className="text">
-          <Col className="column">
-            <div>
-              <h2>piero</h2>
-              <b>delorenzis</b>
-            </div>
-          </Col>
-          <Col className="column">
-            Copyright &copy; {year} Piero De Lorenzis. All Rights Reserved
-          </Col>
-        </Row>
+            <Row className="text">
+                <Col className="column">
+                    <div>
+                        <h2>piero</h2>
+                        <b>delorenzis</b>
+                    </div>
+                </Col>
+                <Col className="column">
+                    Copyright &copy; {year} Piero De Lorenzis. All Rights Reserved
+                </Col>
+            </Row>
 
-    </div>
-  )
+        </div>
+    )
 }
